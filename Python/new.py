e1bb29f4729de75e0ddf48813cd56c6c530c8e1d
queents4 <queents.queents4@gmail.com>
2023-03-16 20:18:37 +0100

    all-Details-Scrits

---
+++ Python/new.py
@@ -0,0 +1,41 @@
+from colorama import init, Fore, Style
+
+init()
+
+print(Fore.RED + "This text is red" + Style.RESET_ALL)
+print(Fore.GREEN + "This text is green" + Style.RESET_ALL)
+#add color for  output terminal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = 2 + 2
+y = 4 + 3
+print("rechteck :", x * y)
+
+print(2, 3, 5, 6, 7, 10, sep=" feri ")  # seb : between of the words write feri or any other word.
+
+print(2, 3, 5, 6, 7, 10, end=" ending")  # end : what want we add  at end of my  print.
+
+print(2, '\n', 5, '\n', 6, '\n', 7, '\n', 10, sep="#")  # schreiben im nächste satz.
+
+
+
+
+
+
+
