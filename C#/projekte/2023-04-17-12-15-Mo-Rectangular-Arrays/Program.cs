688953da7621b215d70f94aa96bd4b3f7b842367
queents4 <queents.queents4@gmail.com>
2023-04-18 19:23:04 +0200

    C#-tag5

---
+++ C#/projekte/2023-04-17-12-15-Mo-Rectangular-Arrays/Program.cs
@@ -0,0 +1,98 @@
+﻿// Erste Dimension = 2; => 2 Elemente
+int[] oneDimensionalArray = { 1, 2 };
+Console.WriteLine(oneDimensionalArray[0]); // => 1
+Console.WriteLine(oneDimensionalArray[1]); // => 2
+
+// Erste Dimension = 4, Zweite Dimension = 2
+// => 4 * 2 = 8 Elemente
+int[,] twoDimensionalArray = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+Console.WriteLine(twoDimensionalArray[2, 1]); // => 6
+Console.WriteLine(twoDimensionalArray[1, 0]); // => 3
+
+
+// Erste Dimension = 3, Zweite Dimension = 4, Dritte Dimension = 2
+// => 3 * 4 * 2 = 24 Elemente
+int[,,] threeDimensionalArray =
+{
+  {
+    { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }
+  },
+  {
+    { 9, 10 }, { 11, 12 }, { 13, 14 }, { 15, 16 }
+  },
+  {
+    { 17, 18 }, { 19, 20 }, { 21, 22 }, { 23, 24 }
+  }
+};
+Console.WriteLine(threeDimensionalArray[1, 2, 0]); // => 13
+
+
+// Erste Dimension = 2, Zweite Dimension = 3, Dritte Dimension = 4, Vierte Dimension = 2
+// => 2 * 3 * 4 * 2 = 48 Elemente, die direkt nacheinander im Speicher stehen.
+int[,,,] fourDimensionalArray =
+{
+  {
+    {
+      { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }
+    },
+    {
+      { 9, 10 }, { 11, 12 }, { 13, 14 }, { 15, 16 }
+    },
+    {
+      { 17, 18 }, { 19, 20 }, { 21, 22 }, { 23, 24 }
+    }
+  },
+  {
+    {
+      { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }
+    },
+    {
+      { 9, 10 }, { 11, 12 }, { 13, 14 }, { 15, 16 }
+    },
+    {
+      { 17, 18 }, { 19, 20 }, { 21, 22 }, { 23, 24 }
+    }
+  }
+};
+Console.WriteLine(fourDimensionalArray[1, 1, 1, 0]); // => 11
+Console.WriteLine(fourDimensionalArray[1, 2, 0, 1]); // => 18
+
+
+int[,] table = new int[5, 4]; // initialisiert alle 20 Elemente mit 0.
+Console.WriteLine(table.GetLength(0)); // => 5
+Console.WriteLine(table.GetLength(1)); // => 4
+Console.WriteLine(table.Length); // => 5 * 4 = 20 Elemente
+Console.WriteLine(table.Rank); // => 2 (zwei Dimensionen)
+
+// Challenge: Initialisiere alle Zellen der Tabelle table, in dem du sie absteigend durchnummerierst.
+int n = 20;
+for (int row = 0; row < table.GetLength(0); ++row)
+{
+  for (int column = 0; column < table.GetLength(1); ++column)
+  {
+    //table[row, column] = table.Length - (row * table.GetLength(1)) - column;
+    table[row, column] = n--;
+  }
+}
+
+Console.WriteLine(table);
+
+string[] names = { "alice", "bob", "charlie", "damian" };
+Array.Reverse(names);
+Console.WriteLine(string.Join(",", names));
+
+// Challenge: Schreibe eine Methode, die die Reihenfolge der Elemente eines Arrays umkehrt.
+// Bemerkung: nutze nur Indizes, Schleifen und Zuweisungen. Das Original-Array soll modifiziert werden.
+
+string[] pets = { "ape", "bear", "cat", "dog" };
+ReverseStringArray(pets);
+Console.WriteLine(string.Join(", ", pets));
+static void ReverseStringArray(string[] array)
+{
+  for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
+  {
+    string temporary = array[i];
+    array[i] = array[j];
+    array[j] = temporary;
+  }
+}
---
+++ C#/projekte/2023-04-17-14-25-Mo-BMI-Tabelle/Program.cs
@@ -0,0 +1,78 @@
+﻿// Challenge:
+// Gib eine BMI Tabelle auf der Kommandozeile aus.
+// - keine Achsenbeschriftungen
+// - nur Vordergrundfarbe setzen
+// - Farbe für Untergewicht, Normalgewicht, Übergewicht, Fettleibigkeit I und Fettleibigkeit II
+// - Körpergröße mit 2 Nachkommastellen
+// - Gewicht ohne Nachkommastellen
+// - BMI Mit einer Nachkommastelle
+// - Speichere die BMI Tabelle zunächst in einem double[,] Array (zweidimensionaler Array)
+// - minimale und maximale Körpergröße sollen frei wählbar sein. Dasselbe gilt für das Gewicht.
+// - Körpergröße wird in 2cm Schritten hochgezählt. Beim Gewicht sind es jeweils 10kg.
+
+PrintBMITable(170, 220, 80, 160);
+
+static void PrintBMITable(int minHeightInCm, int maxHeightInCm, int minWeightInKg, int maxWeightInKg)
+{
+  const int heightStep = 2;
+  const int weightStep = 10;
+
+  int rowCount = (maxHeightInCm - minHeightInCm) / heightStep + 1;
+  int columnCount = (maxWeightInKg - minWeightInKg) / weightStep + 1;
+  double[,] table = new double[rowCount, columnCount];
+
+  FillTable();
+  PrintWeightAxis();
+  PrintTable();
+  Console.ResetColor();
+
+  void FillTable()
+  {
+    for (int height = minHeightInCm, rowIndex = 0; rowIndex < table.GetLength(0); rowIndex++, height += heightStep)
+    {
+      for (int weight = minWeightInKg, columnIndex = 0; columnIndex < table.GetLength(1); columnIndex++, weight += weightStep)
+      {
+        table[rowIndex, columnIndex] = CalculateBMI(height, weight);
+      }
+    }
+  }
+
+  void PrintWeightAxis()
+  {
+    Console.Write(new string(' ', 5));
+    for (int weight = minWeightInKg; weight <= maxWeightInKg; weight += weightStep)
+    {
+      Console.Write($"{weight,5} ");
+    }
+    Console.WriteLine();
+  }
+
+  void PrintTable()
+  {
+    for (int rowIndex = table.GetLength(0) - 1, height = maxHeightInCm; rowIndex >= 0; rowIndex--, height -= heightStep)
+    {
+      Console.ResetColor();
+      Console.Write($"{height / 100.0:F2} ");
+      for (int columnIndex = 0; columnIndex < table.GetLength(1); columnIndex++)
+      {
+        double bmi = table[rowIndex, columnIndex];
+        Console.ForegroundColor = bmi switch
+        {
+          < 18.5 => ConsoleColor.Blue,
+          < 25 => ConsoleColor.Green,
+          < 30 => ConsoleColor.Yellow,
+          < 35 => ConsoleColor.DarkYellow,
+          _ => ConsoleColor.Red
+        };
+        Console.Write($"{bmi,5:F1} ");
+      }
+      Console.WriteLine();
+    }
+  }
+
+}
+
+static double CalculateBMI(int heightInCm, int weightInKg) => weightInKg / Math.Pow(heightInCm / 100.0, 2);
+
+
+
