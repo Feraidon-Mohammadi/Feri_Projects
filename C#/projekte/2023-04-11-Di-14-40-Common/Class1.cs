2321586aadeeb1124a5385260367b675b29a47e0
queents4 <queents.queents4@gmail.com>
2023-04-18 18:20:58 +0200

    C#-Solution+projekte

---
+++ C#/projekte/2023-04-11-Di-14-40-Common/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace _2023_04_11_Di_14_40_Common
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ C#/projekte/2023-04-11-Di-14-55-HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2023_04_11_Di_14_55_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+        }
+    }
+}
