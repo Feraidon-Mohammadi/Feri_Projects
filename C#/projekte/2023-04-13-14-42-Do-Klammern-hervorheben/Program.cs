cd08645d02c144147b5af7d2f14f24d0f94ab598
queents4 <queents.queents4@gmail.com>
2023-04-18 18:58:45 +0200

    tag3

---
+++ C#/projekte/2023-04-13-14-42-Do-Klammern-hervorheben/Program.cs
@@ -0,0 +1,36 @@
+﻿ConsoleColor[] colors = { ConsoleColor.White, ConsoleColor.Yellow, ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Cyan };
+
+
+//foreach (ConsoleColor color in colors)
+//{
+//  Console.ForegroundColor = color;
+//  Console.WriteLine("Test");
+//}
+
+string expression = "((()))()()";
+
+Queue<ConsoleColor> availableColors = new(colors);
+Stack<ConsoleColor> usedColors = new();
+
+foreach (char c in expression)
+{
+  if (c == '(')
+  {
+    ConsoleColor foregroundColor = availableColors.Dequeue();
+    availableColors.Enqueue(foregroundColor);
+    Console.ForegroundColor = foregroundColor;
+    Console.Write("(");
+    usedColors.Push(foregroundColor);
+  }
+  else if (c == ')')
+  {
+    if (usedColors.Count == 0)
+    {
+      Console.WriteLine("Zur schließenden Klammer fehlt eine öffnende Klammer!");
+      return;
+    }
+    ConsoleColor lastColor = usedColors.Pop();
+    Console.ForegroundColor = lastColor;
+    Console.Write(")");
+  }
+}
