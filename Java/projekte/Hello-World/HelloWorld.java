96cb0e730078aad09e7b3d4edc7977861e1d9ad1
queents4 <queents.queents4@gmail.com>
2023-07-02 15:05:45 +0200

    java

---
+++ Java/projekte/Hello-World/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	
+	// Die Laufzeitgebung (JVM) beginnt die Ausführung eines
+	// Programms immer in einer main-Methode.
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+	
+	
+}
